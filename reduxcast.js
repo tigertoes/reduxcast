c6fd7c363ef099d2b32267c50fb5522184316af3
Tiger Toes <tigertoes@users.noreply.github.com>
2014-04-22 22:50:41 +0100

    This is going to be ridiculous
    So, genius me failed to do the basic thing of RTFM of the Chrome Extension API
    and learn about how the different runtimes are seggregated but the DOM is
    shared. When you're trying to get at the page's runtime and you're stuck in your
    own and the global variable you want differs, things get desperate.
    
    So I tried to work around this rubbish and to be honest like a goat ramming a
    dam I might just get there (and if I did, I'm sure there's bounty from Google in
    it). Except it's getting late and I have better things to do. So what is this
    abomination? Well, I can reach the DOM which means I can inject and execute
    abitrary Javascript inside that DOM.
    
    HA! TAKE THAT!
    
    This is only going to get worse.

--- reduxcast.js
+++ reduxcast.js
@@ -1,6 +1,42 @@
-/*
- *
- */
-
 'use strict';
 
+var chromecast_app_id     = '',
+    chromecast_sender_src = 'https://www.gstatic.com/cv/js/sender/v1/cast_sender.js';
+
+var script    = document.createElement('script');
+script.src    = chromecast_sender_src;
+document.head.appendChild(script);
+
+/*
+ * Shout out to Zv_oDD over at http://stackoverflow.com/a/14416259 for this.
+ */
+
+function chromeCastForte() {
+/*LOL
+
+  var attempts = 0,
+      max_attempts = 5;
+  setTimeout(isAvailable, 1000);
+  function isAvailable(){ 
+    console.info("Attempt: " + attempts); 
+    if(attempts >= max_attempts){
+      return;
+    } 
+    if(!chrome.cast || !chrome.cast.isAvailable){ 
+      setTimeout(isAvailable, 1000); return; 
+    } 
+    console.log(chrome);
+  }
+
+LOL*/
+  var here = "LOL";
+  var reobj = new RegExp("/\\*"+here+"\\n[\\s\\S]*?\\n"+here+"\\*/", "m");
+  var str = reobj.exec(chromeCastForte).toString();
+  str = str.replace(new RegExp("/\\*"+here+"\\n",'m'),'').toString();
+  return str.replace(new RegExp("\\n"+here+"\\*/",'m'),'').toString();
+}
+
+var nasty = document.createElement('script');
+nasty.setAttribute('type', 'text/javascript');
+nasty.innerHTML =  chromeCastForte();
+document.head.appendChild(nasty);
