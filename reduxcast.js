9a6b5de627a739dcea846e45b2d51a6bfd6cc431
Tiger Toes <tigertoes@users.noreply.github.com>
2014-04-25 01:06:03 +0100

    This is a big commit that almost gets it working..
    Except almost. You see, all of a sudden my Chromecast (whose name is Claymore,
    don't ask) along with it's best buddy Chrome and the Chromecast extension almost
    don't want me to playback video, it disappears off my local network ENTIRELY
    when I load an example Redux page but the moment I switch to a Youtube page it
    shows up again, just when you need it with a look on its face wondering why you
    think it's doing something wrong.
    
    ... or, a more than likely story is that either my network is bonkers or my code
    is rubbish. Any guesses?

--- reduxcast.js
+++ reduxcast.js
@@ -29,7 +29,6 @@ function chromeCastForte() {
 
   window.onLoad = isAvailable();
 
-  // Check the receiver code is loaded and ready
   function isAvailable(){
     if(attempts >= maxAttempts){
       console.error("Maximum attempts reached");
@@ -44,25 +43,59 @@ function chromeCastForte() {
   }
 
   function sessionListener(e) {
-
+    console.log(e);
   }
 
   function receiverListener(e) {
-
+    if(e === chrome.cast.ReceiverAvailability.AVAILABLE) {
+      console.info("Receiver now available...");
+      var button = document.getElementById("chromecast_button");
+      if(button) {
+        button.onclick = playVideo;
+      } else {
+        console.warn("What? No button?!");
+      }
+    }
   }
 
   function onInitSuccess(){
-
+    console.info("Chromecast successfully setup, let's roll");
   }
 
   function onError(){
-
+    console.warn("Unable to get Chromecast off the ground!");
   }
 
   function setupChromeCast(){
     var sessionRequest = new chrome.cast.SessionRequest(chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID);
     var apiConfig = new chrome.cast.ApiConfig(sessionRequest, sessionListener, receiverListener);
     chrome.cast.initialize(apiConfig, onInitSuccess, onError);
+  }
+
+  function playVideo(){
+    var source = document.getElementsByTagName('source');
+    if(source.length <=0) {
+      console.error("Video can't be found, huh?");
+      return;
+    }
+    var url = source[0].getAttribute("src");
+    console.info("Sending " + url + " outbound!");
+    chrome.cast.requestSession(function(session){
+      var mediaInfo = new chrome.cast.media.MediaInfo(url);
+      var request = new chrome.cast.media.LoadRequest(mediaInfo);
+      session.loadMedia(request,
+      onMediaDiscovered.bind(this, 'loadMedia'),
+      function(e){
+        console.error("Unable to cast: " + e.code);
+      }); //FIXME callback hell
+
+    }, function(e) {
+      console.error('Error: ' + e.code);
+    });
+  }
+
+  function onMediaDiscovered(how, media) {
+    console.log(media);
   }
 
 LOL*/
