c98b97f3bcfa48c6348fc134033f1bed838a804c
Tiger Toes <tigertoes@users.noreply.github.com>
2014-05-02 00:18:10 +0100

    Move the here-doc shenanigans into a separate file
    Instead of doing that shimming hackery, using a separate file and content
    loading it works, and means we can stop doing bad things.

---
+++ extension.js
@@ -0,0 +1,29 @@
+/**
+ *                   __                           __
+ *    ________  ____/ /_  ___  ___________ ______/ /_
+ *   / ___/ _ \/ __  / / / / |/_/ ___/ __ `/ ___/ __/
+ *  / /  /  __/ /_/ / /_/ />  </ /__/ /_/ (__  ) /_
+ * /_/   \___/\__,_/\__,_/_/|_|\___/\__,_/____/\__/
+ *
+ */
+'use strict';
+
+var chromecastSenderSrc = 'https://www.gstatic.com/cv/js/sender/v1/cast_sender.js';
+
+if(document.location.pathname.match(/^\/asset/)) {
+  // Inject the sender script
+  var script = document.createElement('script');
+  script.src = chromecastSenderSrc;
+  document.head.appendChild(script);
+
+  // Inject playback button in
+  var header = document.getElementsByTagName('h2')[0],
+      headerTitle = header.innerText,
+      imageSrc = '<img id="chromecast_button" src="' + chrome.extension.getURL("cast_off.png") + '"/>';
+  header.innerHTML = headerTitle + imageSrc;
+
+  // Inject our own JS
+  var reduxcast = document.createElement('script');
+  reduxcast.src = chrome.extension.getURL('reduxcast.js');
+  document.head.appendChild(reduxcast);
+}
--- reduxcast.js
+++ reduxcast.js
@@ -1,129 +1,80 @@
-/**
- *                   __                           __
- *    ________  ____/ /_  ___  ___________ ______/ /_
- *   / ___/ _ \/ __  / / / / |/_/ ___/ __ `/ ___/ __/
- *  / /  /  __/ /_/ / /_/ />  </ /__/ /_/ (__  ) /_
- * /_/   \___/\__,_/\__,_/_/|_|\___/\__,_/____/\__/
- *
- */
 'use strict';
 
-var chromecastSenderSrc = 'https://www.gstatic.com/cv/js/sender/v1/cast_sender.js';
+var attempts    = 0,
+    maxAttempts = 30,
+    delay       = 1000,
+    isReady     = false,
+    media       = false,
+    buttonId    = 'chromecast_button';
 
-if(document.location.pathname.match(/^\/asset/)) {
-  // Inject the sender script
-  var script = document.createElement('script');
-  script.src = chromecastSenderSrc;
-  document.head.appendChild(script);
+window.onLoad = isAvailable();
+document.getElementById(buttonId).onclick = playVideo;
 
-  // Inject playback button in
-  var header = document.getElementsByTagName('h2')[0],
-      headerTitle = header.innerText,
-      imageSrc = '<img id="chromecast_button" src="' + chrome.extension.getURL("cast_off.png") + '"/>';
-  header.innerHTML = headerTitle + imageSrc;
-
-  // Inject our own JS
-  var nasty = document.createElement('script');
-  nasty.setAttribute('type', 'text/javascript');
-  nasty.innerHTML = chromeCastForte();
-  document.head.appendChild(nasty);
+function isAvailable(){
+  if(attempts >= maxAttempts){
+    console.error("Maximum attempts reached");
+    return;
+  }
+  if(!chrome.cast || !chrome.cast.isAvailable){
+    attempts++;
+    setTimeout(isAvailable, delay);
+    return;
+  }
+  setupChromeCast();
 }
 
-/**
- * A brief explanation of what is going on here:
- * Content script extensions in Chrome seggregate the scopes of extension
- * runtime and page runtime - which means we can't access global variables like
- * chrome.cast in the page from this end. To work around it, we have to inject
- * JS into the page's DOM and have it evaluated. Given we need to push quite a
- * bit in, the below method essentially is a here-doc, something Javascript is
- * as yet able to support.
- *
- * Shout out to Zv_oDD over at http://stackoverflow.com/a/14416259 for this.
- */
-function chromeCastForte() {
-/*LOL
-
-  var attempts    = 0,
-      maxAttempts = 30,
-      delay       = 1000,
-      isReady     = false,
-      media       = false,
-      buttonId    = 'chromecast_button';
-
-  window.onLoad = isAvailable();
-  document.getElementById(buttonId).onclick = playVideo;
-
-  function isAvailable(){
-    if(attempts >= maxAttempts){
-      console.error("Maximum attempts reached");
-      return;
-    }
-    if(!chrome.cast || !chrome.cast.isAvailable){
-      attempts++;
-      setTimeout(isAvailable, delay);
-      return;
-    }
-    setupChromeCast();
-  }
-
-  function sessionListener(e) {
-    console.info('Session Listener initialised');
-  }
-
-  function receiverListener(e) {
-    console.info('Receiver Listener initialised');
-    if(e !== chrome.cast.ReceiverAvailability.AVAILABLE) return;
-    isReady = true;
-  }
-
-  function onInitSuccess(e) {
-    console.info('Chromecast initialised!');
-  }
-
-  function onError(e) {
-    //TODO change/hide the icon?
-  }
-
-  function onMediaDiscovered(how, newMedia) {
-    media = newMedia;
-  }
-
-  function setupChromeCast() {
-    var sessionRequest = new chrome.cast.SessionRequest(chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID);
-    var apiConfig = new chrome.cast.ApiConfig(sessionRequest,
-      sessionListener,
-      receiverListener);
-    chrome.cast.initialize(apiConfig, onInitSuccess, onError);
-  }
-
-  function playVideo(){
-    if(!isReady) return;
-    var source = document.getElementsByTagName('source');
-    if(source.length <=0) {
-      console.error("Video can't be found, huh?");
-      return;
-    }
-    var url = source[0].getAttribute("src");
-    console.info("Sending " + url + " outbound!");
-    chrome.cast.requestSession(function(session){
-      var mediaInfo = new chrome.cast.media.MediaInfo(url);
-      mediaInfo.contentType = 'video/mp4';
-      var request = new chrome.cast.media.LoadRequest(mediaInfo);
-      session.loadMedia(request,
-      onMediaDiscovered.bind(this, 'loadMedia'),
-      function(e){
-        console.error("Unable to cast: " + e.code);
-      }); //FIXME callback hell
-
-    }, function(e) {
-      console.error('Error: ' + e.code);
-    });
-  }
-
-LOL*/
-  var here = "LOL";
-  var reobj = new RegExp("/\\*"+here+"\\n[\\s\\S]*?\\n"+here+"\\*/", "m");
-  var str = reobj.exec(chromeCastForte).toString();
-  str = str.replace(new RegExp("/\\*"+here+"\\n",'m'),'').toString();
-  return str.replace(new RegExp("\\n"+here+"\\*/",'m'),'').toString();
+function sessionListener(e) {
+  console.info('Session Listener initialised');
 }
+
+function receiverListener(e) {
+  console.info('Receiver Listener initialised');
+  if(e !== chrome.cast.ReceiverAvailability.AVAILABLE) return;
+  isReady = true;
+}
+
+function onInitSuccess(e) {
+  console.info('Chromecast initialised!');
+}
+
+function onError(e) {
+  //TODO change/hide the icon?
+}
+
+function onMediaDiscovered(how, newMedia) {
+  media = newMedia;
+}
+
+function setupChromeCast() {
+  var sessionRequest = new chrome.cast.SessionRequest(chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID);
+  var apiConfig = new chrome.cast.ApiConfig(sessionRequest,
+    sessionListener,
+    receiverListener);
+  chrome.cast.initialize(apiConfig, onInitSuccess, onError);
+}
+
+function playVideo(){
+  if(!isReady) return;
+  var source = document.getElementsByTagName('source');
+  if(source.length <=0) {
+    console.error("Video can't be found, huh?");
+    return;
+  }
+  var url = source[0].getAttribute("src");
+  console.info("Sending " + url + " outbound!");
+  chrome.cast.requestSession(function(session){
+    var mediaInfo = new chrome.cast.media.MediaInfo(url);
+    mediaInfo.contentType = 'video/mp4';
+    var request = new chrome.cast.media.LoadRequest(mediaInfo);
+    session.loadMedia(request,
+    onMediaDiscovered.bind(this, 'loadMedia'),
+    function(e){
+      console.error("Unable to cast: " + e.code);
+    }); //FIXME callback hell
+
+  }, function(e) {
+    console.error('Error: ' + e.code);
+  });
+}
+
+
